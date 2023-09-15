9796229d20412a4bdb252e604cadbd64f659cdef
MaheshDurgam9030 <122861464+MaheshDurgam9030@users.noreply.github.com>
2023-09-15 22:37:58 +0530

    test

---
+++ Datatypes/Dictionaries.py
@@ -0,0 +1,9 @@
+
+
+def add(a,b):
+    c= a+b
+    #print( " sum of two values is " , c)
+
+obj = add(10,20)
+
+print(obj)
---
+++ Datatypes/Flow_control.py
@@ -0,0 +1,64 @@
+# name = input("Enter the value of name ")
+#
+# if name.title() == 'Sreeni'.title():
+#     print("Hello",name.upper(), " Good morning")
+#
+# elif name =='Chandan':
+#     print("Hello", name, " Good morning")
+# elif name=='Raghav':
+#     print("Hello", name, " Good morning")
+# else:
+#     print("Hello guest good morning")
+#
+#
+# print("This is out if-eli-else stmt")
+
+
+layer = input("Enter the layer")
+#bronze, silver, Gold --
+
+if layer.strip() == 'Raw':
+    print('count')
+    print('duplicate')
+    print('columnvalue')
+elif layer == "Raw":
+    print('count1')
+    print('columnvalue1')
+elif layer == 'silver':
+    print('count')
+    print('duplicate')
+    print('columnvalue')
+    print('transformation validation, scd')
+elif layer == 'gold':
+    pass
+
+print("outside layer exe")
+
+
+
+
+
+# number = int(input("Enter number "))
+# if number == 1:
+#     print("Entered number is one")
+# elif number ==2:
+#     print("Entered number is two")
+# elif number ==3:
+#     print("Entered number is three")
+# elif number>3:
+#     print("Entered number is greate than 3")
+# else:
+#     print("enter number either 0 or negative number")
+
+
+# def dodo(b):
+#     b=b+'2'
+#     print("before mul",b)
+#     b= b*3
+#     print("after mul",b)
+#
+# dodo(b="whale")
+#
+# if 0.8+0.1 == 0.9:
+#     print("True")
+
